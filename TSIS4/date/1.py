e7d1faeb8e9fda491cf5b177da21c4bedbfc7e5f
aluanamalik <125024127+aluanamalik@users.noreply.github.com>
2023-05-01 02:09:05 +0600

    Update 1.py

--- TSIS4/date/1.py
+++ TSIS4/date/1.py
@@ -2,3 +2,5 @@ from datetime import date, timedelta
 dt = date.today() - timedelta(5)
 print('Current Date :', date.today())
 print('5 days before Current Date :', dt)
+
+#OR print(date.today() - timedelta(5))
